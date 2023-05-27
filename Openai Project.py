6c761d5342a03b0f3739ee70e88fa072740336a2
akkkshat07 <119446445+akkkshat07@users.noreply.github.com>
2023-05-27 15:05:23 +0530

    Update Openai Project.py

--- Openai Project.py
+++ Openai Project.py
@@ -1,12 +1,12 @@
 import requests
 
 def search(query):
-    url = "https://www.googleapis.com/customsearch/v1"  # Google Custom Search API URL
-    api_key = "AIzaSyArmR0VWxfyy5urqsX0nI-Ngb8L3iLGVe8"  # Replace with your own API key
-    cx = "7647ac75be2b045ea"  # Replace with your own search engine ID
+    url = "https://www.googleapis.com/customsearch/v1" #Search API url
+    api_key = "AIzaSyArmR0VWxfyy5urqsX0nI-Ngb8L3iLGVe8"  #API key
+    search_engine_id = "7647ac75be2b045ea"           #Search engine id
     params = {
         "key": api_key,
-        "cx": cx,
+        "search_engine_id": search_engine_id,
         "q": query
     }
     response = requests.get(url, params=params)
@@ -21,7 +21,7 @@ def get_answer(query):
             title = top_result["title"]
             snippet = top_result["snippet"]
             return f"{title}\n\n{snippet}"
-    return "Sorry, I couldn't find an answer to your question."
+    return "Sorry, I couldn't find an answer to your question, please try asking again"
 
 # Main loop
 while True:
